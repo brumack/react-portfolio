e47f99e5da95c15b4201cbd951979a640434c320
Brad Rumack <brad.rumack@gmail.com>
2019-03-15 16:38:43 -0700

    Updates

---
+++ precache-manifest.503a40ee0438d2bd27ed639e7e5bda23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "92d12d74cb108ef141fd",
+    "url": "/react-portfolio/static/js/main.7ee46803.chunk.js"
+  },
+  {
+    "revision": "e880066450df8094ec53",
+    "url": "/react-portfolio/static/js/2.fafb079b.chunk.js"
+  },
+  {
+    "revision": "92d12d74cb108ef141fd",
+    "url": "/react-portfolio/static/css/main.77c01725.chunk.css"
+  },
+  {
+    "revision": "5430ae40b73fa0a5a9d36a14d9db5939",
+    "url": "/react-portfolio/index.html"
+  }
+];
