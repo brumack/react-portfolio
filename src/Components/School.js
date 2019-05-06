af4c5139ff8838c8565548a88f1e37c2b73ed256
Brad Rumack <brad.rumack@gmail.com>
2019-05-05 18:50:24 -0700

    remove description from education object

--- src/Components/School.js
+++ src/Components/School.js
@@ -11,7 +11,6 @@ export default props => {
         <br />
         {renderDetails(props.detail)}
       </p>
-      <p>{props.description}</p>
     </React.Fragment>
   )
 }
--- src/lib/schools.js
+++ src/lib/schools.js
@@ -2,27 +2,19 @@ export default [
   {
     name: 'Free Code Camp',
     image: './images/icons/fcc.png',
-    date: '2017',
-    detail: ['Front-end Developer Certification',' APIs and Microservices Certification', 'JavaScript Algorithms and Data Structures Certification'],
-    description: `The FCC curriculum consists of extensive education in both front-end and back-end technologies.' +
-    ' The program also involves completing several projects, some of which you'll find available in my portfolio.`
+    date: '2017-',
+    detail: ['Front-end Developer Certification',' APIs and Microservices Certification', 'JavaScript Algorithms and Data Structures Certification']
   },
   {
     name: 'Udemy',
     image: './images/icons/udemy.png',
     date: '2017 -',
-    detail: ['Multiple Courses'],
-    description: `Through Udemy I've studied various topics such as Node.js, React.js, Python, MySQL, and Wordpress`
+    detail: ['Multiple Courses']
   },
   {
     name: 'CSU Long Beach',
     image: './images/icons/csulb.jpeg',
     date: 'August 2010 - March 2013',
-    detail: ['Finance Major', 'Deans List'],
-    description: 'My initial move to California was to attend classes at CSULB, where I studied finance.' +
-      ' My experience was excellent. An unfortunate health condition sprung itself, so I was' +
-      ' forced to withdraw in the Spring of 2013, and when finances ran dry, I had to ' +
-      ' return to Arizona. I had the goal of returning to California ever since, and I finally' +
-      ' made it.'
+    detail: ['Finance Major', 'Deans List']
   }
 ]
